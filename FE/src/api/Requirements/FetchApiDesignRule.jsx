deee0259e2e4cc5a88a2d3550c2cddc93220cec7
PhucLocNguyen <loc19992003bca@gmail.com>
2024-06-17 20:57:40 +0700

    Fixing bug in Step 2 and add Login Google API

--- FE/src/api/Requirements/FetchApiDesignRule.jsx
+++ FE/src/api/Requirements/FetchApiDesignRule.jsx
@@ -4,9 +4,9 @@ import api from '../instance.jsx'
 
 const FetchApiDesignRuleById = async (id) => {
    try {
-      const response = await api.get(`/DesignRule/${id}`);
+      const response = await api.get(`/DesignRule?TypeOfJewelleryId=${id}`);
       const designRuleById = response.data;
-      return designRuleById; // Directly return the data from the response
+      return designRuleById[0]; // Directly return the data from the response
    } catch (error) {
       console.error(error);
       return {}; // Return an empty array or handle the error as needed

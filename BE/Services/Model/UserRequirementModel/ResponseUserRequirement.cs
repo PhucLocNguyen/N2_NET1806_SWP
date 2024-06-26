2f3b18af8d91d83b92bf8ea7891cb0124afb7dee
NguyenBaDat <bmbadat2234@gmail.com>
2024-06-26 22:06:13 +0700

    Delete

--- BE/Services/Model/UserRequirementModel/ResponseUserRequirement.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace API.Model.UserRequirementModel
-{
-    public class ResponseUserRequirement
-    {
-        public int UsersId { get; set; }
-        public int RequirementId { get; set; }
-    }
-}
--- BE/Services/Model/WarrantyCardModel/WarrantyCardDTO.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace API.Model.WarrantyCardModel
-{
-    public class WarrantyCardDTO
-    {
-        public int WarrantyCardId { get; set; }
-        public string Title { get; set; } = null!;
-        public string Description { get; set; }
-    }
-}

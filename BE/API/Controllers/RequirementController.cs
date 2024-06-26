6be21258ff2cf64ec744e8686976497ca743a395
NguyenBaDat <bmbadat2234@gmail.com>
2024-06-26 22:07:48 +0700

    Update newest version

---
+++ BE/API/Controllers/RequirementController.cs
@@ -0,0 +1,164 @@
+﻿using API.Model.DesignModel;
+using API.Model.MasterGemstoneModel;
+using API.Model.RequirementModel;
+using API.Model.UserModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Entity;
+using SWP391Project.Services.WorkingBoard.Hubs;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RequirementController : ControllerBase
+    {
+        private readonly UnitOfWork _unitOfWork;
+        private readonly IHubContext<WorkingHub> _hubContext;
+        public RequirementController(UnitOfWork unitOfWork, IHubContext<WorkingHub> hubContext)
+        {
+            _unitOfWork = unitOfWork;
+            _hubContext = hubContext;
+        }
+
+        [HttpGet]
+        public IActionResult SearchBlog([FromQuery] RequestSearchRequirementModel requestSearchRequirementModel)
+        {
+            var sortBy = requestSearchRequirementModel.SortContent != null ? requestSearchRequirementModel.SortContent?.sortRequirementBy.ToString() : null;
+            var sortType = requestSearchRequirementModel.SortContent != null ? requestSearchRequirementModel.SortContent?.sortRequirementType.ToString() : null;
+            Expression<Func<Requirement, bool>> filter = x =>
+                (string.IsNullOrEmpty(requestSearchRequirementModel.Status) || x.Status.Contains(requestSearchRequirementModel.Status)) &&
+                (x.Size == requestSearchRequirementModel.Size|| requestSearchRequirementModel.Size == null) &&
+                (x.DesignId == requestSearchRequirementModel.DesignId || requestSearchRequirementModel.DesignId == null) &&
+                x.MaterialPriceAtMoment >= requestSearchRequirementModel.FromMaterialPriceAtMoment &&
+                (x.MaterialPriceAtMoment <= requestSearchRequirementModel.ToMaterialPriceAtMoment || requestSearchRequirementModel.ToMaterialPriceAtMoment == null) &&
+                x.WeightOfMaterial >= requestSearchRequirementModel.FromWeightOfMaterial &&
+                (x.WeightOfMaterial <= requestSearchRequirementModel.ToWeightOfMaterial || requestSearchRequirementModel.ToWeightOfMaterial == null) &&
+                x.StonePriceAtMoment >= requestSearchRequirementModel.FromStonePriceAtMoment &&
+                (x.StonePriceAtMoment <= requestSearchRequirementModel.ToStonePriceAtMoment || requestSearchRequirementModel.ToStonePriceAtMoment == null) &&
+                x.MachiningFee >= requestSearchRequirementModel.FromMachiningFee &&
+                (x.MachiningFee <= requestSearchRequirementModel.ToMachiningFee || requestSearchRequirementModel.ToMachiningFee == null) &&
+                x.TotalMoney >= requestSearchRequirementModel.FromTotalMoney &&
+                (x.TotalMoney <= requestSearchRequirementModel.ToTotalMoney || requestSearchRequirementModel.ToTotalMoney == null);
+            Func<IQueryable<Requirement>, IOrderedQueryable<Requirement>> orderBy = null;
+
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                if (sortType == SortRequirementTypeEnum.Ascending.ToString())
+                {
+                    orderBy = query => query.OrderBy(p => EF.Property<object>(p, sortBy));
+                }
+                else if (sortType == SortDesignTypeEnum.Descending.ToString())
+                {
+                    orderBy = query => query.OrderByDescending(p => EF.Property<object>(p, sortBy));
+                }
+            }
+            var reponseDesign = _unitOfWork.RequirementRepository.Get(
+                filter,
+                orderBy,
+                includeProperties: "",
+                pageIndex: requestSearchRequirementModel.pageIndex,
+                pageSize: requestSearchRequirementModel.pageSize
+                ).Select(x => x.toRequirementDTO());
+            return Ok(reponseDesign);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetRequirementById(int id)
+        {
+            var Requirement = _unitOfWork.RequirementRepository.GetByID(id);
+            if (Requirement == null)
+            {
+                return NotFound("Requiremnet is not existed");
+            }
+
+            return Ok(Requirement.toRequirementDTO());
+        }
+
+        [HttpPost]
+        public IActionResult CreateRequirement(RequestCreateRequirementModel requestCreateRequirementModel)
+        {
+            var error = "";
+            var properties = typeof(RequestCreateRequirementModel).GetProperties();
+
+            foreach (var property in properties)
+            {
+                if (property.PropertyType == typeof(decimal))
+                {
+                    var value = property.GetValue(requestCreateRequirementModel);
+                    if ((decimal)value < 0)
+                    {
+                        error = property.Name + " must be positive number";
+                        break;
+                    }
+                }
+            }
+            if (!string.IsNullOrEmpty(error))
+            {
+                return BadRequest(error);
+            }
+            var Requirement = requestCreateRequirementModel.toRequirementEntity();
+            _unitOfWork.RequirementRepository.Insert(Requirement);
+            _unitOfWork.Save();
+            var requirementId = Requirement.RequirementId;
+            _hubContext.Clients.All.SendAsync("ReceiveOrderCreate", requirementId);
+            return Ok("Create requirement successfully");
+        }
+
+        
+        [HttpPut("{id}")]
+        public IActionResult UpdateRequirement([FromRoute]int id,RequestCreateRequirementModel requestCreateRequirementModel)
+        {
+            var existedRequirement = _unitOfWork.RequirementRepository.GetByID(id);
+            if (existedRequirement == null)
+            {
+                return NotFound("Requiremnet is not existed");
+            }
+            existedRequirement.Status = requestCreateRequirementModel.Status;
+            existedRequirement.ExpectedDelivery = requestCreateRequirementModel.ExpectedDelivery!=null? DateOnly.FromDateTime((DateTime)requestCreateRequirementModel.ExpectedDelivery):null;
+            existedRequirement.Size = requestCreateRequirementModel.Size;
+            existedRequirement.DesignId = (int)requestCreateRequirementModel.DesignId;
+            existedRequirement.Design3D = requestCreateRequirementModel.Design3D;
+            existedRequirement.WeightOfMaterial = requestCreateRequirementModel.WeightOfMaterial;
+            existedRequirement.MaterialPriceAtMoment = requestCreateRequirementModel.MaterialPriceAtMoment;
+            existedRequirement.StonePriceAtMoment = requestCreateRequirementModel.StonePriceAtMoment;
+            existedRequirement.MachiningFee = requestCreateRequirementModel.MachiningFee;
+            existedRequirement.TotalMoney = requestCreateRequirementModel.TotalMoney;
+            existedRequirement.CustomerNote = requestCreateRequirementModel.CustomerNote;
+            existedRequirement.StaffNote = requestCreateRequirementModel.StaffNote;
+            _unitOfWork.RequirementRepository.Update(existedRequirement);
+            _unitOfWork.Save();
+            _hubContext.Clients.All.SendAsync("ReceiveOrderUpdate", id);
+            return Ok("Update Requirement successfully");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteRequirement(int id)
+        {
+            var existedRequirement = _unitOfWork.RequirementRepository.GetByID(id);
+            if (existedRequirement == null)
+            {
+                return NotFound("Requiremnet is not existed");
+            }
+            _unitOfWork.RequirementRepository.Delete(existedRequirement);
+            _unitOfWork.Save();
+            return Ok();
+        }
+
+        [HttpGet("GetRequirementByRole")]
+
+        public IActionResult GetRequirement(int userId, string status)
+        {
+            var RequirementByStatus = _unitOfWork.RequirementRepository.Get(filter: x=>x.Status.Equals(status)).ToList();
+            var UserRequirementByUserId = _unitOfWork.UserRequirementRepository.Get(filter: x=>x.UsersId == userId).Select(x=>x.RequirementId).ToList();
+            var Result = RequirementByStatus.Where(x=>UserRequirementByUserId.Contains(x.RequirementId)).Select(x=>x.toRequirementDTO()).ToList();
+            return Ok(Result);
+        }
+
+        
+    }
+}
---
+++ BE/API/Controllers/UserController.cs
@@ -0,0 +1,242 @@
+﻿using API.Model.TypeOfJewellryModel;
+using API.Model.UserModel;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Email;
+using Repositories.Entity;
+using Repositories.Token;
+using System.Linq.Expressions;
+using System.Net;
+using static Repositories.Email.EmailService;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UnitOfWork _unitOfWork;
+        private readonly IToken _tokenService;
+        private readonly IEmailService _emailService;
+        public UserController(UnitOfWork unitOfWork, IToken tokenService, IEmailService emailService)
+        {
+            _unitOfWork = unitOfWork;
+            _tokenService = tokenService;
+            _emailService = emailService;
+        }
+        [HttpPost("registerForCustomer")]
+        public async Task<IActionResult> Register([FromBody] RequestRegisterAccount requestRegisterAccount)
+        {
+            
+                if (checkDuplicateUsername(requestRegisterAccount.Username))
+                {
+                    return BadRequest("Username is existed");
+                }
+            var existUserHaveSameEmail = _unitOfWork.UserRepository.Get(filter: x => x.Email.Equals(requestRegisterAccount.Email));
+                if (existUserHaveSameEmail.Count() >0)
+                {
+                    return BadRequest("Email has already been registered");
+                }
+                if (!requestRegisterAccount.Password.Equals(requestRegisterAccount.PasswordConfirm))
+                {
+                    return BadRequest("PasswordConfirm is not correct");
+                }
+                _emailService.SaveInCache(requestRegisterAccount);
+                return Ok("Please check email to verify your email");
+        }
+
+        [HttpPost("registerForAdmin")]
+        //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = Role.Customer)]
+        public  IActionResult RegisterForAdmin([FromBody] RequestRegisterAccount requestRegisterAccount, [FromQuery] RoleEnum roleEnum)
+        {
+            try
+            {
+                if (!ModelState.IsValid)
+                    return BadRequest(ModelState);
+                if (checkDuplicateUsername(requestRegisterAccount.Username))
+                {
+                    return BadRequest("Username is existed");
+                }
+                if (!requestRegisterAccount.Password.Equals(requestRegisterAccount.PasswordConfirm))
+                {
+                    return BadRequest("PasswordConfirm is not correct");
+                }
+                var role = roleEnum.ToString() != "0" ? roleEnum.ToString() : "Customer";
+                var roleEntity = _unitOfWork.RoleRepository.Get(filter: x => x.Name.Equals(role)).FirstOrDefault();
+                var registerAccount = requestRegisterAccount.toUserEntity(roleEntity);
+                _unitOfWork.UserRepository.Insert(registerAccount);
+                _unitOfWork.Save();
+                //return Ok(registerAccount.toUserDTO());
+                return Ok("Regist Successfully");
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException is SqlException sqlException && sqlException.Number == 2627)
+                {
+                    return BadRequest("Email has already been registered");
+                }
+                else
+                {
+                    return Problem("Something appear when registing", statusCode: 500);
+                }
+            }
+           
+        }
+        [HttpPost("loginForCustomer")]
+        public  async Task<IActionResult> LoginForCustomer(RequestLoginAccount loginDTO)
+        {
+            try
+            {
+                Expression<Func<Users, bool>> filter = x =>
+                    (x.Username.Equals(loginDTO.Username));
+                var user = _unitOfWork.UserRepository.Get(
+                    includes: m => m.Role
+                    ).Where(x=> x.Username.Equals(loginDTO.Username, StringComparison.Ordinal)).FirstOrDefault();
+
+                if (user == null) { return BadRequest("Invalid Username"); }
+
+                if (!BCrypt.Net.BCrypt.Verify(loginDTO.Password, user.Password)) { return BadRequest("Password incorrect"); }
+
+                if(user.Role.Name == RoleConst.Customer)
+                {
+                    return Ok(await _tokenService.CreateToken(user));
+                }
+                return BadRequest("You do not have permission to access this page");
+            }
+            catch (NullReferenceException ex)
+            {
+                return BadRequest("The account does not register");
+            }
+        }
+
+        [HttpPost("loginForStaff")]
+        public async Task<IActionResult> LoginForStaff(RequestLoginAccount loginDTO)
+        {
+            try
+            {
+                Expression<Func<Users, bool>> filter = x =>
+                    (x.Username.Equals(loginDTO.Username));
+                var user = _unitOfWork.UserRepository.Get(
+                    includes: m => m.Role
+                    ).Where(x => x.Username.Equals(loginDTO.Username, StringComparison.Ordinal)).FirstOrDefault();
+
+                if (user == null) { return BadRequest("Invalid Username"); }
+
+                if (!BCrypt.Net.BCrypt.Verify(loginDTO.Password, user.Password)) { return BadRequest("Password incorrect"); }
+
+
+                if (user.Role.Name != RoleConst.Customer)
+                {
+                    return Ok(await _tokenService.CreateToken(user));
+                }
+                return BadRequest("You do not have permission to access this page");
+            }
+            catch (NullReferenceException ex)
+            {
+                return BadRequest("The account does not register");
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll([FromQuery] string RoleFromInput = null)
+        {
+            Expression<Func<Users, bool>> filter = x =>
+                (string.IsNullOrEmpty(RoleFromInput) || x.Role.Name.Contains(RoleFromInput));
+            var Users = _unitOfWork.UserRepository.Get(filter);
+            return Ok(Users);
+        }
+
+        [HttpGet("Username")]
+        public IActionResult GetByUsername(string username)
+        {
+            Expression<Func<Users, bool>> filter = x =>
+                (string.IsNullOrEmpty(username) || x.Username.Equals(username));
+            var Users = _unitOfWork.UserRepository.Get(filter);
+            return Ok(Users);
+        }
+
+        [HttpGet]
+        [Route("GetUserByRoleInRequirement")]
+        public IActionResult GetUserByRoleInRequirement([FromQuery] RoleEnum RoleFromInput, [FromQuery] int requirementId)
+        {
+
+            var users = _unitOfWork.UserRequirementRepository
+            .Get(filter: x => x.RequirementId == requirementId, includeProperties: "User")
+            .Select(ur => ur.User).Where(x => x.RoleId == (int)RoleFromInput)
+            .ToList();
+            return Ok(users);
+        }
+
+        [HttpPut]
+        public IActionResult UpdateProfile(int userId, UserDTO editUser)
+        {
+            try
+            {
+                var existedUser = _unitOfWork.UserRepository.GetByID(userId);
+                if (existedUser == null)
+                {
+                    return NotFound("User does not found");
+                }
+                existedUser.Name = editUser.Name;
+                existedUser.Phone = editUser.Phone;
+                existedUser.Image = editUser.Image;
+                _unitOfWork.UserRepository.Update(existedUser);
+                _unitOfWork.Save();
+                return Ok("Edit profile succesfully");
+            }
+            catch (DbUpdateException ex)
+            {
+                if (ex.InnerException is SqlException sqlException && sqlException.Number == 2627)
+                {
+                    return Conflict("Email has already been registered");
+                }
+                else
+                {
+                    return Problem("Something appear when registing", statusCode: 500);
+                }
+            }
+        }
+
+        [HttpPost("VerifyEmail")]
+        public async Task<IActionResult> SendEmail([FromBody] string VerifyCodeFromUser)
+        {
+
+            try
+            {
+                var result = _emailService.VerifyCode(VerifyCodeFromUser);
+
+               
+                return result switch
+                {
+                    VerifyResult.Success => Ok("Regist Successfully"),
+                    VerifyResult.Expired => BadRequest("Verification code has expired. Please sign up again"),
+                    VerifyResult.Invalid => BadRequest("Invalid verification code. Please try again"),
+                };
+
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+            private bool checkDuplicateUsername(string username)
+        {
+            bool check = false;
+            var existedAccount = _unitOfWork.UserRepository.Get();
+            foreach (var item in existedAccount)
+            {
+                if (item.Username.Equals(username)) 
+                {
+                    check = true;
+                    break;
+                }
+            }
+            return check;
+        }
+    }
+}

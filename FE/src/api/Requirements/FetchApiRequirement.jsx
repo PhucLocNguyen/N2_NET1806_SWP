a73b5848ee5f00ca553e8483b1d045a064a5d01e
Tydy1410 <thienntse171341@fpt.edu.vn>
2024-06-30 16:42:29 +0700

    Create TodoPopup and DonePopup for planninglist

--- FE/src/api/Requirements/FetchApiRequirement.jsx
+++ FE/src/api/Requirements/FetchApiRequirement.jsx
@@ -1,40 +1,41 @@
-import api from '../instance.jsx'
-import axiosConfigHeader from '../AxiosConfigHeader.jsx';
+import api from "../instance.jsx";
+import axiosConfigHeader from "../AxiosConfigHeader.jsx";
 
-const FetchApiRequirementOpeningOrder= async (id) => {
-   try {
-      const response = await api.get(`/Requirement?Status=${id}`);
-      const requirementWithStatus = response.data;
-      return requirementWithStatus; 
-   } catch (error) {
-      console.error(error);
-      return []; 
-   }
-}
+const FetchApiRequirementOpeningOrder = async (id) => {
+  try {
+    const response = await api.get(`/Requirement?Status=${id}`);
+    const requirementWithStatus = response.data;
+    return requirementWithStatus;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
 
-const FetchApiRequirementByStatus = async (currentUserId,statusId) => {
-   try {
-      const response = await api.get(`/Requirement/GetRequirementByRole`,{
-         axiosConfigHeader,params:{
-            status: statusId,
-            userId: currentUserId
-         }
-      });
-      const requirementWithStatus = response.data;
-      return requirementWithStatus; 
-   } catch (error) {
-      console.error(error);
-      return []; 
-   }
-}
-const FetchApiRequirementById= async (id) => {
-   try {
-      const response = await api.get(`/Requirement/${id}`);
-      const requirementById = response.data;
-      return requirementById; 
-   } catch (error) {
-      console.error(error);
-      return []; 
-   }
-}
-export { FetchApiRequirementOpeningOrder,FetchApiRequirementByStatus,FetchApiRequirementById}
+const fetchApiRequirementByStatus = async (status) => {
+  try {
+    const response = await api.get(
+      `/Requirement?Status=${status}`,
+      axiosConfigHeader
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+const FetchApiRequirementById = async (id) => {
+  try {
+    const response = await api.get(`/Requirement/${id}`);
+    const requirementById = response.data;
+    return requirementById;
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
+};
+export {
+  FetchApiRequirementOpeningOrder,
+  fetchApiRequirementByStatus,
+  FetchApiRequirementById,
+};

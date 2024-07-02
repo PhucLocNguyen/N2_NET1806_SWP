637049cac8688fc56f935ce9bd482e69ee3b500a
NguyenBaDat <bmbadat2234@gmail.com>
2024-07-02 22:28:52 +0700

    Delete

--- BE/Services/Model/ConversationModel/RequestCreateConversation.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace API.Model.ConversationModel
-{
-    public class RequestCreateConversation
-    {
-        public int userId1 { get; set; }
-        public int userId2 { get; set; }
-    }
-}

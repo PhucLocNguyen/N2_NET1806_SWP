81504d9af9adcdee67e6ce105cb66faec1d89ee7
NguyenBaDat <bmbadat2234@gmail.com>
2024-07-02 22:29:58 +0700

    Fix payment save data in database

---
+++ BE/API/Controllers/StonesController.cs
@@ -0,0 +1,178 @@
+﻿using API.Model.MasterGemstoneModel;
+using API.Model.StonesModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Entity;
+using System.Linq.Expressions;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StonesController : ControllerBase
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public StonesController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        [HttpGet("GetTotalRecords")]
+        public IActionResult GetTotalRecords([FromQuery] RequestSearchStonesModel requestSearchStonesModel)
+        {
+            Expression<Func<Stones, bool>> filter = x =>
+                (string.IsNullOrEmpty(requestSearchStonesModel.Kind) || x.Kind.Contains(requestSearchStonesModel.Kind)) &&
+                (x.Size == requestSearchStonesModel.Size || requestSearchStonesModel.Size == null) &&
+                x.Quantity >= requestSearchStonesModel.FromQuantity &&
+                (x.Quantity <= requestSearchStonesModel.ToQuantity || requestSearchStonesModel.ToQuantity == null) &&
+                x.Price >= requestSearchStonesModel.FromPrice &&
+                (x.Price <= requestSearchStonesModel.ToPrice || requestSearchStonesModel.ToPrice == null);
+
+            var totalRecords = _unitOfWork.StoneRepository.Count(filter);
+
+            var response = new
+            {
+                TotalRecords = totalRecords
+            };
+
+            return Ok(response);
+        }
+        [HttpGet]
+        public IActionResult SearchStones([FromQuery] RequestSearchStonesModel requestSearchStonesModel)
+        {
+            var sortBy = requestSearchStonesModel.SortContent != null ? requestSearchStonesModel.SortContent?.sortStonetBy.ToString() : null;
+            var sortType = requestSearchStonesModel.SortContent != null ? requestSearchStonesModel.SortContent?.sortStonesType.ToString() : null;
+            Expression<Func<Stones, bool>> filter = x =>
+                (string.IsNullOrEmpty(requestSearchStonesModel.Kind) || x.Kind.Contains(requestSearchStonesModel.Kind)) &&
+                (x.Size == requestSearchStonesModel.Size || requestSearchStonesModel.Size == null) &&
+                x.Quantity >= requestSearchStonesModel.FromQuantity &&
+                (x.Quantity <= requestSearchStonesModel.ToQuantity || requestSearchStonesModel.ToQuantity == null) &&
+                x.Price >= requestSearchStonesModel.FromPrice &&
+                (x.Price <= requestSearchStonesModel.ToPrice || requestSearchStonesModel.ToPrice == null);
+            Func<IQueryable<Stones>, IOrderedQueryable<Stones>> orderBy = null;
+
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                if (sortType == SortStonesTypeEnum.Ascending.ToString())
+                {
+                    orderBy = query => query.OrderBy(p => EF.Property<object>(p, sortBy));
+                }
+                else if (sortType == SortStonesTypeEnum.Descending.ToString())
+                {
+                    orderBy = query => query.OrderByDescending(p => EF.Property<object>(p, sortBy));
+                }
+            }
+            var reponseDesign = _unitOfWork.StoneRepository.Get(
+                filter,
+                orderBy,
+                pageIndex: requestSearchStonesModel.pageIndex,
+                pageSize: requestSearchStonesModel.pageSize,
+                x=>x.Designs
+                ).Select(x=>x.toStonesDTO());
+            return Ok(reponseDesign);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetStonesById(int id)
+        {
+            var Stones = _unitOfWork.StoneRepository.GetByID(id,p=>p.Designs);
+            if(Stones == null)
+            {
+                return NotFound("Stones is not existed");
+            }
+            return Ok(Stones.toStonesDTO());
+        }
+        [HttpPost]
+        public IActionResult CreateStones(RequestCreateStonesModel requestCreateStonesModel)
+        {
+            var error = "";
+            var properties = typeof(RequestCreateStonesModel).GetProperties();
+
+            foreach (var property in properties)
+            {
+                if (property.PropertyType == typeof(decimal))
+                {
+                    var value = property.GetValue(requestCreateStonesModel);
+                    if ((decimal)value < 0)
+                    {
+                        error = property.Name + " must be positive number";
+                        break;
+                    }
+                }
+                if (property.PropertyType == typeof(string))
+                {
+                    var value = property.GetValue(requestCreateStonesModel);
+                    if (string.IsNullOrEmpty((string)value))
+                    {
+                        error = property.Name + " must be not plank";
+                        break;
+                    }
+                }
+            }
+            if (!string.IsNullOrEmpty(error))
+            {
+                return BadRequest(error);
+            }
+            var getStoneSize = _unitOfWork.StoneRepository.Get(filter: x => (x.Kind == requestCreateStonesModel.Kind) && (x.Size == requestCreateStonesModel.Size) && (x.Quantity == requestCreateStonesModel.Quantity)).FirstOrDefault();
+            if (getStoneSize != null)
+            {
+                return BadRequest("Stone with this size and quantity had existed");
+            }
+            try
+            {
+                var stones = requestCreateStonesModel.toStonesEntity();
+                _unitOfWork.StoneRepository.Insert(stones);
+                _unitOfWork.Save();
+                return Ok("Create successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Create failed");
+            }
+        }
+        [HttpPut]
+        public IActionResult UpdateStones(int id, RequestCreateStonesModel requestCreateStonesModel)
+        {
+            var existedStonesUpdate = _unitOfWork.StoneRepository.GetByID(id);
+            if (existedStonesUpdate == null)
+            {
+                return NotFound("Stones is not existed");
+            }
+            existedStonesUpdate.Kind = requestCreateStonesModel.Kind;
+            existedStonesUpdate.Price = requestCreateStonesModel.Price;
+            existedStonesUpdate.Quantity = requestCreateStonesModel.Quantity;
+            existedStonesUpdate.Size = requestCreateStonesModel.Size;
+            _unitOfWork.StoneRepository.Update(existedStonesUpdate);
+            _unitOfWork.Save();
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteStones(int id)
+        {
+            var existedStonesUpdate = _unitOfWork.StoneRepository.GetByID(id);
+            if(existedStonesUpdate == null)
+            { 
+                return NotFound("Stones is not existed");
+            }
+            _unitOfWork.StoneRepository.Delete(existedStonesUpdate);
+            try
+            {
+                _unitOfWork.Save();
+            }
+            catch (DbUpdateException ex)
+            {
+                if (_unitOfWork.IsForeignKeyConstraintViolation(ex))
+                {
+                    return BadRequest("Cannot delete this item because it is referenced by another entity.");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok("Delete Successfully");
+        }
+    }
+}
---
+++ BE/API/Controllers/TypeOfJewelleryController.cs
@@ -0,0 +1,138 @@
+﻿using API.Model.TypeOfJewellryModel;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Repositories;
+using Repositories.Entity;
+using System.Linq.Expressions;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TypeOfJewelleryController : ControllerBase
+    {
+        private readonly UnitOfWork _unitOfWork;
+
+        public TypeOfJewelleryController(UnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult SearchJewellery([FromQuery] RequestSearchTypeOfJewelleryModel requestSearchTypeOfJewelleryModel)
+        {
+
+            var sortBy = requestSearchTypeOfJewelleryModel.SortContent != null ? requestSearchTypeOfJewelleryModel.SortContent?.sortTypeOfJewelleryBy.ToString() : null;
+            var sortType = requestSearchTypeOfJewelleryModel.SortContent != null ? requestSearchTypeOfJewelleryModel.SortContent?.sortTypeOfJewelleryType.ToString() : null;
+            Expression<Func<TypeOfJewellery, bool>> filter = x =>
+                (string.IsNullOrEmpty(requestSearchTypeOfJewelleryModel.Name) || x.Name.Contains(requestSearchTypeOfJewelleryModel.Name));
+            Func<IQueryable<TypeOfJewellery>, IOrderedQueryable<TypeOfJewellery>> orderBy = null;
+
+            if (!string.IsNullOrEmpty(sortBy))
+            {
+                if (sortType == SortTypeOfJewelleryTypeEnum.Ascending.ToString())
+                {
+                    orderBy = query => query.OrderBy(p => EF.Property<object>(p, sortBy));
+                }
+                else if (sortType == SortTypeOfJewelleryTypeEnum.Descending.ToString())
+                {
+                    orderBy = query => query.OrderByDescending(p => EF.Property<object>(p, sortBy));
+                }
+            }
+            var reponseJewellery = _unitOfWork.TypeOfJewellryRepository.Get(filter,
+                orderBy,
+                pageIndex: requestSearchTypeOfJewelleryModel.pageIndex,
+                pageSize: requestSearchTypeOfJewelleryModel.pageSize,
+                includes: m =>m.Designs).Select(x=>x.toTypeOfJewelleryDTO());
+            return Ok(reponseJewellery);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetTypeOfJewelleryById(int id)
+        {
+            var TypeOfJewellery = _unitOfWork.TypeOfJewellryRepository.GetByID(id,p=>p.Designs);
+            if (TypeOfJewellery == null)
+            {
+                return NotFound("Type of jewellery is not existed");
+            }
+
+            return Ok(TypeOfJewellery.toTypeOfJewelleryDTO());
+        }
+
+        [HttpPost]
+        public IActionResult CreateTypeOfJewellery(RequestCreateTypeOfJewelleryModel requestTypeOfJewelleryModel)
+        {
+            try
+            {
+                var ExistTypeOfJewellery = _unitOfWork.TypeOfJewellryRepository.Get(filter: x => x.Name.Equals(requestTypeOfJewelleryModel.Name)).FirstOrDefault();
+                if (ExistTypeOfJewellery != null)
+                {
+                    return BadRequest("This Jewellery does exist");
+                }
+                var TypeOfJewellery = new TypeOfJewellery()
+                {
+                    Name = requestTypeOfJewelleryModel.Name,
+                    Image = requestTypeOfJewelleryModel.Image,
+                };
+                _unitOfWork.TypeOfJewellryRepository.Insert(TypeOfJewellery);
+                _unitOfWork.Save();
+                return Ok("Create successfully");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Create failed");
+            }
+           
+        }
+
+        [HttpPut]
+        public IActionResult UpdateTypeOfJewellery(int id, RequestCreateTypeOfJewelleryModel requestTypeOfJewelleryModel)
+        {
+            try
+            {
+                var existedTypeOfJewellery = _unitOfWork.TypeOfJewellryRepository.GetByID(id);
+                if (existedTypeOfJewellery == null)
+                {
+                    return NotFound("Type of jewellery is not existed");
+                }
+                existedTypeOfJewellery.Name = requestTypeOfJewelleryModel.Name;
+                _unitOfWork.TypeOfJewellryRepository.Update(existedTypeOfJewellery);
+                _unitOfWork.Save();
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return BadRequest("Update failed");
+            }
+            
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteTypeOfJewellery(int id)
+        {
+            var existedTypeOfJewellery = _unitOfWork.TypeOfJewellryRepository.GetByID(id);
+            if (existedTypeOfJewellery == null)
+            {
+                return NotFound("Type of jewellery is not existed");
+            }
+            _unitOfWork.TypeOfJewellryRepository.Delete(existedTypeOfJewellery);
+            try
+            {
+                _unitOfWork.Save();
+            }
+            catch (DbUpdateException ex)
+            {
+                if (_unitOfWork.IsForeignKeyConstraintViolation(ex))
+                {
+                    return BadRequest("Cannot delete this item because it is referenced by another entity.");
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok("Delete Successfully");
+        }
+    }
+}

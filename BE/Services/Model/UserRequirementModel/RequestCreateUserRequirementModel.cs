7269522af6ef896ed68429b3c0c7dc94cd2cbe46
NguyenBaDat <bmbadat2234@gmail.com>
2024-07-06 11:17:27 +0700

    Delete

--- BE/Services/Model/UserRequirementModel/RequestCreateUserRequirementModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace API.Model.UserRequirementModel
-{
-    public class RequestCreateUserRequirementModel
-    {
-        public int UsersId { get; set; }
-        public int RequirementId { get; set; }
-    }
-}
